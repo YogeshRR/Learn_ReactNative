6ae54bebf385959ab88fb83807a78f6072a97133
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-01-31 06:06:08 +0530

    16 Styling React Native Apps

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Another Text</Text>
-      <Text>On Two Device</Text>
-      <Button title='Tap Here'/>
+      <Text style={styles.dummyText}>Another Text</Text>
+      <Text style={styles.dummyText} >On Two Device</Text>
+      <Button title='Tap Here' />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText : {
+    margin : 15,
+    borderColor : 'blue',
+    borderWidth : 2,
+    padding : 15,
   },
 });
