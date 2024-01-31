d71e8807b313d9d139592acb73e84b4c120a69bf
Yogesh Raut <yogesh@Yogeshs-MacBook-Air.local>
2024-01-31 05:44:58 +0530

    15 Working with Core Component

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Another Text</Text>
       <Text>On Two Device</Text>
+      <Button title='Tap Here'/>
       <StatusBar style="auto" />
     </View>
   );
